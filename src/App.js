6eadc337e1bdfb10d358f8bc3f13be8810ba5642
Mariana Kecova <mariana.kecova@gisat.cz>
2021-08-26 17:55:19 +0200

    new layers added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import DeckGL from "@deck.gl/react";
-import { GeoJsonLayer } from "@deck.gl/layers";
+import { GeoJsonLayer, ColumnLayer } from "@deck.gl/layers";
+import { HeatmapLayer, HexagonLayer } from "@deck.gl/aggregation-layers";
 import { StaticMap } from "react-map-gl";
+import { ShapefileLoader } from "@loaders.gl/shapefile";
 import { JSONLoader } from "@loaders.gl/json";
 import chroma from "chroma-js";
 import { load } from "@loaders.gl/core";
@@ -15,9 +17,12 @@ const INITIAL_VIEW_STATE = {
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibWFyaWRhbmkiLCJhIjoiSGF2TGdwZyJ9.B0N8ybRGG38wmRK_VfxPoA";
 
+const SHP_URL = "./data/manila_buildings_clip_larger_than_250.shp";
+
 const URLS = [
-  "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/los/142_decimated.json",
+  // "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/los/142_decimated.json",
   "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/los/32.json",
+  // "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/los/142.json",
   // "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/vertg/142.json",
   // "https://ptr.gisat.cz/ftpstorage/applications/emsn091Manila/interferometry/vertg/32.json",
 ];
@@ -26,18 +31,31 @@ let colorScale = chroma
   .scale(["#fda34b", "#ff7882", "#c8699e", "#7046aa", "#0c1db8", "#2eaaac"])
   .domain([-30, 10]);
 
+let heightColorScale = chroma
+  .scale(["#A0FAB4", "#88CA97", "#6E9B78", "#536E59"])
+  .domain([0, 20]);
+
 export default class App extends Component {
   state = {
     jsonData: [],
+    shpData: [],
   };
 
   componentDidMount() {
     this._loadData().then((data) => {
       this.setState({ jsonData: data });
     });
+    this._loadShpData().then((shpData) => {
+      this.setState({ shpData });
+    });
   }
 
   componentWillUnmount() {}
+
+  _loadShpData = async () => {
+    let promisedData = await load(SHP_URL, ShapefileLoader);
+    return promisedData.data;
+  };
 
   _loadData = async () => {
     let promisedData = [];
@@ -56,7 +74,77 @@ export default class App extends Component {
   render() {
     let layers = [];
 
+    if (this.state.shpData.length > 0) {
+      layers.push(
+        new GeoJsonLayer({
+          id: "geojson-layer",
+          data: this.state.shpData,
+          pickable: false,
+          filled: true,
+          extruded: true,
+          getFillColor: (d) => [
+            ...heightColorScale(Math.random() * 20).rgb(),
+            255,
+          ],
+          getElevation: (d) => Math.random() * 20,
+          elevationScale: 3,
+        })
+      );
+    }
+
     if (this.state.jsonData.length > 0) {
+      // layers.push(
+      //   new HeatmapLayer({
+      //     id: "heatmapLayer",
+      //     data: this.state.jsonData,
+      //     getPosition: (d) => d.geometry.coordinates,
+      //     getWeight: (d) => d.properties.vel_avg,
+      //     aggregation: "MEAN",
+      //   })
+      // );
+
+      // layers.push(
+      //   new HexagonLayer({
+      //     id: "hexagon-layer",
+      //     data: this.state.jsonData,
+      //     pickable: true,
+      //     extruded: true,
+      //     radius: 200,
+      //     elevationScale: 4,
+      //     getColorWeight: (d) => d.properties.vel_avg,
+      //     colorAggregation: "MEAN",
+      //     getElevationWeight: (d) => d.properties.vel_avg,
+      //     elevationAggregation: "SUM",
+      //     elevationDomain: [-30, 10],
+      //     colorDomain: [-30, 10],
+      //     colorScale: [
+      //       "#fda34b",
+      //       "#ff7882",
+      //       "#c8699e",
+      //       "#7046aa",
+      //       "#0c1db8",
+      //       "#2eaaac",
+      //     ],
+      //     getPosition: (d) => d.geometry.coordinates,
+      //   })
+      // );
+
+      // layers.push(
+      //   new ColumnLayer({
+      //     id: "column-layer",
+      //     data: this.state.jsonData,
+      //     diskResolution: 12,
+      //     radius: 250,
+      //     extruded: true,
+      //     pickable: true,
+      //     elevationScale: 500,
+      //     getPosition: (d) => d.geometry.coordinates,
+      //     getFillColor: (d) => colorScale(d.properties.vel_avg).rgb(),
+      //     getLineColor: [0, 0, 0],
+      //     getElevation: (d) => 40 + d.properties.vel_avg,
+      //   })
+      // );
+
       layers.push(
         new GeoJsonLayer({
           id: "geojson-layer",
@@ -69,7 +157,7 @@ export default class App extends Component {
           lineWidthScale: 20,
           lineWidthMinPixels: 2,
           getFillColor: (d) => [...colorScale(d.properties.vel_avg).rgb(), 180],
-          getPointRadius: 100,
+          getPointRadius: 20,
         })
       );
     }
@@ -80,7 +168,9 @@ export default class App extends Component {
           initialViewState={INITIAL_VIEW_STATE}
           controller={true}
           layers={layers}
-          getTooltip={({ object }) => object && `${object.properties.vel_avg}`}
+          // getTooltip={({ object }) =>
+          //   object && `Vel avg: ${object.properties.vel_avg}`
+          // }
         >
           <StaticMap
             mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
